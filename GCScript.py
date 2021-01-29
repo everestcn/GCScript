f84de9d855e6c830dfedafdb5404b595f3a60774
1887415157 <61649614+everestcn@users.noreply.github.com>
2021-01-29 21:33:35 +0800

    Add a logic Mod.
    none

--- GCScript.py
+++ GCScript.py
@@ -4,15 +4,13 @@
 import zipfile
 import os
 import math
-import tkinter
 import time
 import sys
 import random
-
 def readfile(file):
     try:    
         
-        files = open(file+".gc")               
+        files = open(file)               
         line = files.readline()               
         while line: 
             
@@ -28,18 +26,53 @@ def sys_init_start(DE=False, GU=False, NE=True):
     GUI = GU
     NEW = NE
 
+
 #settings
 global read_sct
 global ids
 DEBUG = False
-VER = "0.0.1"
-KEYWORDS = ["when", "if", "outprint", "SaveAndQuit", "using", "setv", "/", "help", "get_input", "pause"]
+VER = "0.0.3"
+KEYWORDS = ["when", "if", "outprint", "SaveAndQuit", "using", "setv", "/", "help", "get_input", "pause", "+", "-", "*", "/", "**", "if", "c_value"]
+Ifwords = ["then"]
 VLAUETYPES = ["string", "int", "float", "boolen", "bin"]
+math_types = ["/", "*", "-", "+", "**"]
 value_branches = {0:"string", 1:"int", 2:"float", 3:"boolen", 4:"bin"}
 helps = None
 users_value = {}
 #settings
+global contion
+def if_branch(condition):
 
+    global contion
+    n1, n2 = '', ''
+    contion = None
+    ctn = condition.split("==")
+    if ctn[0] in users_value.keys():
+        n1 = users_value[ctn[0]]
+                
+    if ctn[1] in users_value.keys():
+        n2 = users_value[ctn[1]]
+                
+                
+    if n1 == '':
+        try:
+            n1 = int(ctn[0])
+        except ValueError:
+            print("can't find value!")
+    if n2 == '':
+        try:
+            n2 = int(ctn[1])
+        except ValueError:
+            print("can't find value!")
+    if n1 == n2:
+        contion = True
+        return None
+    if n1 != n2:
+        contion = False
+        return None
+    else:
+        print("faild:")
+        return None
 def help_system(help_things=None):
     try:    
         
@@ -71,36 +104,81 @@ def sys_exit_code(mode=False):
     if mode == True:
         pass
 def scripts(script=None):
+    global contion
     global ids
     if script == None:return None
     else:
         global read_sct
         read_sct = script
+        if script == "c_d":
+            print(users_value)
         for keywords in KEYWORDS:
             if keywords in script:
+                if keywords == "c_value":
+                    branch("[", "]")
+                    if ids == 2:
+                        if script[8:len(script)-1] in users_value:
+                            get_change = input("input for change value "+script[8:len(script)-1]+".type '' to quit:")
+                            if get_change != '':
+                            
+                                users_value[script[7:len(script)-1]] = get_change
+                            else:
+                                return None
+                        
+                if keywords == "if":
+                    branch("(", ")")
+                    if ids == 2:
+                        if_branch(script[script.index('(')+1:script.index(')')])
+                        if script[len(script)-1:] == "{":
+                            if_under_cmd = []
+                            while 1:
+                                
+                                cmd = input("[in if("+script[script.index('(')+1:script.index(')')]+")]>>>")
+                                if cmd == "}":
+                                    if contion != True:
+                                        return None
+                                    else:
+                                        for run_times in if_under_cmd:
+                                            scripts(run_times)
+                                        return None
+                                else:
+                                    if_under_cmd.append(cmd)
+                                
+                                
+                                    
                 if keywords == "pause":
                     branch("(", ")")
                     if ids == 2:
-                        try:
-                            time.sleep(int(script[6:len(script)-1]))
-                        except:
-                            print("faild:None")
+                        pause_time = int(script[6:len(script)-1])
+                        time.sleep(pause_time)
                     else:print("faild:missing ( or )")
+                if keywords == "+" or keywords == "-" or keywords == "*" or keywords == "/" or keywords == "**":
+                    try:
+                        print(str(eval(script)))
+                        return 0
+                    except TypeError:
+                        print("faild:you must type the number, not string.")
+                        print("\033[0;31;40m\tFAILD\033[0m")
+                        return 0
                 if keywords == "get_input":
                     branch('"', '"')
                     if ids == 2:
                         ipt = input(script[11:len(script)-1])
-                        print("INPUT:"+str(ipt))
-                    else:print('faild:missing "')
+                    else:
+                        print('faild:missing "')
+                        print("\033[0;31;40m\tFAILD\033[0m")
                 if keywords == "using":
                     branch("(", ")")
-                    if ids == 2:
-                        readfile(script[6:len(script)-1])
-                    else:print("faild:missing ( or )")
+                    if ids == 2:readfile(script[6:len(script)-1])
+                    else:
+                        print("faild:missing ( or )")
+                        print("\033[0;31;40m\tFAILD\033[0m")
                 if keywords == "help":
                     branch('(', ')')
                     if ids == 2:help_system()
-                    else:print("faild:missing ( or )")
+                    else:
+                        print("faild:missing ( or )")
+                        print("\033[0;31;40m\tFAILD\033[0m")
                     
                 if keywords == "outprint":
                     branch('"')
@@ -110,52 +188,62 @@ def scripts(script=None):
               
                     else:
                         print('faild:missing " ')
-                        print("FAILD")
+                        print("\033[0;31;40m\tFAILD\033[0m")
                 if keywords == "SaveAndQuit":
                     branch('(', ')')
                     if ids == 2:
                         print("exit with code 0")
                         sys.exit()
-                    else:print("faild:missing ( or )")
+                    else:
+                        print("faild:missing ( or )")
+                        print("\033[0;31;40m\tFAILD\033[0m")
                 if keywords == "setv":
                     branch('[', ']')
                     if ids == 2:
                         value_branch = 0
-                        for _ in range(5):
-                            if script[5:len(script)-1] == value_branches[value_branch]:
-                                print(value_branches[value_branch])
-                                name = input("input for your value name:")
-                                for u_values in users_value:
-                                    if name in u_values:
-                                        print(users_value[name])
-                                        break
-                                    else:
-                                        users_value.update(name)
-                                break
-                            else:
-                                if value_branch < 4:
-                                    value_branch += 1
-                                else:print("faild:can't find value type "+script[5:len(script)-1])
+                        if " " in script[5:len(script)-1]:  
+                            print("faild:you can't type ' ' in value name")
+                            print("\033[0;31;40m\tFAILD\033[0m")
+                            return None
+                        
+                        else:
+                            if script[5:len(script)-1] in users_value:
+                                print(users_value[script[5:len(script)-1]])
+                                return None
+                            value = input("input for your value:")
+                            users_value[script[5:len(script)-1]] = value
+                            return None
+                                
+                                    
+
+                             
+                    
                     else:
                         print("faild:missing [ or ]")
-                        print("FAILD")
+                        print("\033[0;31;40m\tFAILD\033[0m")
         for u_values in users_value:
             if script == u_values:
                 print(users_value[script])
                 break
-            else:
-                pass
+            else:pass
                 
+def control_on_terminal():    
+    print("GCScript v0.01\nusing help() to get more imformation")                 
+    while True:
+        sys_exit_code(mode=True)
+        ream = input(">>>")
+        scripts(ream)   
+
+
                         
-
-print("GCScript v0.01")
-print("using help() to get more imformation")                                 
-while True:
-
-    sys_exit_code(mode=True)
-    ream = input(">>>")
-    scripts(ream)                 
+if __name__ == "GCScript":pass
+if __name__ == "__main__":control_on_terminal()
              
                     
                 
 
+
+
+
+
+x
